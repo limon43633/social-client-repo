67df143d19a34a5a061d14fc04e70e8cf98a8214
Hassibur Rahman <md9178558@gmail.com>
2025-11-18 14:16:35 +0600

    added some new page and update firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";   // 🔥 YOU MISSED THIS
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,5 +14,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// 🔥 Correctly export auth
+export const auth = getAuth(app);
 
 export default app;
